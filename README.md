b60d99a54bfc9628e598b309211af1ac8aea33f3
pcbimon <patipat.che@mahidol.edu>
2024-12-23 20:18:25 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngx-dropzone-angular
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/pcbimon/ngx-dropzone-angular)
---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  
+  files: File[] = [];
+
+	onSelect(event) {
+		console.log(event);
+		this.files.push(...event.addedFiles);
+	}
+
+	onRemove(event) {
+		console.log(event);
+		this.files.splice(this.files.indexOf(event), 1);
+	}
+}
